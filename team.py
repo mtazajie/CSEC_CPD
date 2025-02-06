55de4fde96a00e400e5d0f7b4201760d413e0833
Mihret Tesfaye <167402700+mtazajie@users.noreply.github.com>
2025-02-06 15:49:47 +0300

    Update team.py

--- team.py
+++ team.py
@@ -1,15 +1,8 @@
-# Read the matrix
-matrix = [list(map(int, input().split())) for _ in range(5)]
+n = int(input())
+count = 0
+for i in range(n):
+    petya, vasya, tonya = map(int, input().split())
+    if petya + vasya + tonya >= 2:
+        count += 1
 
-# Find the position of '1'
-for i in range(5):
-    for j in range(5):
-        if matrix[i][j] == 1:
-            row, col = i, j
-            break
-
-# Calculate the minimum number of moves
-moves = abs(row - 2) + abs(col - 2)
-
-# Output the result
-print(moves)
+print(count)

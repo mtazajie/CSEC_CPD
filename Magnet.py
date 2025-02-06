73834c4704307b9094a971e4e01dc103310851e3
Mihret Tesfaye <167402700+mtazajie@users.noreply.github.com>
2025-02-06 16:22:29 +0300

    Create Magnet.py

---
+++ Magnet.py
@@ -0,0 +1,11 @@
+n = int(input().strip())  
+prev = input().strip()  
+groups = 1  
+
+for i in range(n - 1):
+    curr = input().strip()
+    if curr != prev:
+        groups += 1  
+    prev = curr  
+
+print(groups)
